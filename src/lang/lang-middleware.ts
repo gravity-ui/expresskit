5f904f2abdf80d6da6da0372b2a19e60f22cd00f
Andrey Melikhov <melikhov@yandex-team.ru>
2024-10-16 16:19:08 +0300

    fix: refactoring

--- src/lang/lang-middleware.ts
+++ src/lang/lang-middleware.ts
@@ -11,44 +11,46 @@ export function setupLangMiddleware(appCtx: AppContext, expressApp: Express) {
     }
 
     const {defaultLang, allowLanguages, langQueryParamName} = languageConfig;
-    if (allowLanguages && allowLanguages.length > 0 && defaultLang) {
-        expressApp.use((req, _res, next) => {
-            setLang({lang: defaultLang, ctx: req.ctx});
-
-            if (languageConfig.getLangByHostname) {
-                const langByHostname = languageConfig.getLangByHostname(req.hostname);
-
-                if (langByHostname) {
-                    setLang({lang: langByHostname, ctx: req.ctx});
-                }
-            } else {
-                const tld = req.hostname.split('.').pop();
-                const langByTld =
-                    tld && languageConfig.langByTld ? languageConfig.langByTld[tld] : undefined;
-
-                if (langByTld) {
-                    setLang({lang: langByTld, ctx: req.ctx});
-                }
-            }
-
-            if (req.headers['accept-language']) {
-                const langByHeader = acceptLanguage.pick(
-                    allowLanguages,
-                    req.headers['accept-language'],
-                    {loose: true},
-                );
-
-                if (langByHeader) {
-                    setLang({lang: langByHeader, ctx: req.ctx});
-                }
-            }
-
-            const langQuery = langQueryParamName && req.query[langQueryParamName];
-            if (langQuery && typeof langQuery === 'string' && allowLanguages.includes(langQuery)) {
-                setLang({lang: langQuery, ctx: req.ctx});
-            }
-
-            return next();
-        });
+    if (!(allowLanguages && allowLanguages.length > 0 && defaultLang)) {
+        return;
     }
+    expressApp.use((req, _res, next) => {
+        const langQuery = langQueryParamName && req.query[langQueryParamName];
+        if (langQuery && typeof langQuery === 'string' && allowLanguages.includes(langQuery)) {
+            setLang({lang: langQuery, ctx: req.ctx});
+            return next();
+        }
+
+        setLang({lang: defaultLang, ctx: req.ctx});
+
+        if (languageConfig.getLangByHostname) {
+            const langByHostname = languageConfig.getLangByHostname(req.hostname);
+
+            if (langByHostname) {
+                setLang({lang: langByHostname, ctx: req.ctx});
+            }
+        } else {
+            const tld = req.hostname.split('.').pop();
+            const langByTld =
+                tld && languageConfig.langByTld ? languageConfig.langByTld[tld] : undefined;
+
+            if (langByTld) {
+                setLang({lang: langByTld, ctx: req.ctx});
+            }
+        }
+
+        if (req.headers['accept-language']) {
+            const langByHeader = acceptLanguage.pick(
+                allowLanguages,
+                req.headers['accept-language'],
+                {loose: true},
+            );
+
+            if (langByHeader) {
+                setLang({lang: langByHeader, ctx: req.ctx});
+            }
+        }
+
+        return next();
+    });
 }
